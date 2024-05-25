9e4ea5f007e1703a8abee000da5042beae84d950
SPFENIX <cgofenix@hotmail.com>
2024-05-24 20:28:48 -0500

    declaracion de la matriz y variables.

---
+++ Parcial/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    static int[,] tablero = new int[0 , 0]; 
+    static int posX = 0, posY = 0;
+    static bool escKeyPressed = false;
+    private static void Main(string[] args)
+    {
+        
+    }
+}
